f71fdbfe7cffb46400a6c033f5e1a1bd584aae6f
Milan <rmilanrajkovic@gmail.com>
2023-11-23 11:38:37 +0100

    Set action bar for data binding fragments

--- app-databinding/src/main/java/tv/brid/videos/MainActivity.kt
+++ app-databinding/src/main/java/tv/brid/videos/MainActivity.kt
@@ -2,14 +2,31 @@ package tv.brid.videos
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        supportFragmentManager.findFragmentById(R.id.main_nav_host)
+            ?.findNavController()
+            ?.let {
+                navController = it
+                setupActionBarWithNavController(it)
+            }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
